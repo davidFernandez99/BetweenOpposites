30be80c77c6af4c9459f5af5f6e6c3e4311e01a7
ojimenmu <ojimenmu@gmail.com>
2019-03-19 19:44:00 +0100

    Commit Inicial
    Creación del proyecto

---
+++ app/src/main/java/grup05/pis2018/ub/edu/betweenopposites/MainActivity.kt
@@ -0,0 +1,18 @@
+package grup05.pis2018.ub.edu.betweenopposites
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+/**
+ * Projecte Integrat de Software
+ * Creat per:
+ *  Óscar Jiménez
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
