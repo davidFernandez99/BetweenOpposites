7b37b24921709870cc18f5f512000abf64f77178
ojimenmu <ojimenmu@gmail.com>
2019-03-21 09:35:59 +0100

    #5 Creado el boton para la creación de partidas de dos jugadores, junto con el tag del identificador i el textView que nos muestra la ID de la partida.
    Signed-off-by: Óscar Jiménez <ojimenmu@gmail.com>

--- app/src/main/java/grup05/pis2018/ub/edu/betweenopposites/PantallaDosJugadores.kt
+++ app/src/main/java/grup05/pis2018/ub/edu/betweenopposites/PantallaDosJugadores.kt
@@ -3,7 +3,7 @@ package grup05.pis2018.ub.edu.betweenopposites
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class pantallaDosJugadores : AppCompatActivity() {
+class PantallaDosJugadores : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
