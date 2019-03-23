4590ee65e22fccf96505a2a5d970215aac7fc42d
david <davidfernandezfernandez.1999@gmail.com>
2019-03-23 13:32:12 +0100

    Un jugsdor #7

--- app/src/main/java/grup05/pis2018/ub/edu/betweenopposites/UnJugador.kt
+++ app/src/main/java/grup05/pis2018/ub/edu/betweenopposites/UnJugador.kt
@@ -3,13 +3,10 @@ package grup05.pis2018.ub.edu.betweenopposites
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-
-
 class UnJugador : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_un_jugador)
-
     }
 }
