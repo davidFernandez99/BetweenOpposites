5805fa1d5e3ea7ce1718f049330f5d10b74e26ae
Óscar Jiménez <ojimenmu@gmail.com>
2019-05-16 17:11:34 +0200

    Cambio de PC : Método draw() de Sala empezado

--- app/src/main/java/grup05/pis2018/ub/edu/betweenopposites/Model/Sala.kt
+++ app/src/main/java/grup05/pis2018/ub/edu/betweenopposites/Model/Sala.kt
@@ -1,5 +1,7 @@
 package grup05.pis2018.ub.edu.betweenopposites.Model
 
+import android.graphics.Canvas
+
 abstract class Sala(id_sala: Int, matrixSala: Array<Array<Objeto?>>) {
 
     // Mantiene un identificador de la sala en el nivel
@@ -19,12 +21,23 @@ abstract class Sala(id_sala: Int, matrixSala: Array<Array<Objeto?>>) {
     var objetos: ArrayList<Objeto> = ArrayList<Objeto>()
 
 
+    // MÉTODOS PPARA COGER Y MODIFICAR POSICIONES EN LA MATRIZ
     /**
      * Devuelve un objeto guardado en una posición en concreto de la sala.
      */
     fun getObjetofromSala(i: Int, j: Int): Objeto {
         val objeto: Objeto = matrixSala[i][j]!!
         return objeto
+    }
+
+    /**
+     * Introduce un objeto a la sala.
+     */
+    fun setObjetoinSala(objeto: Objeto){
+        // Distribuye las diferentes formas de guardar un objeto
+        if(objeto is Muro || objeto is Suelo) {
+            matrixSala[i][j] = objeto
+        }
     }
 
     // MÉTODOS PARA AÑADIR COSAS A LA SALA
@@ -34,14 +47,67 @@ abstract class Sala(id_sala: Int, matrixSala: Array<Array<Objeto?>>) {
      * Añade una puerta al array de puertas
      */
     fun anadirPuerta(puerta: Puerta) {
+        // No solo se añada a la lista sino también a la matriz
         puertas.add(puerta)
+        // Sincroniza las puertas que hay en la matriz con las se la lista
+        syncPuertas()
+    }
+
+    /**
+     * Se encarga de sincronizar las puertas que hay en matriz con las que hay en la lista
+     */
+    private fun syncPuertas() : Boolean{
+        // Recogemos las puertas que hay en la matriz
+        var diferentes: Boolean = false
+
+        var puertasRecogidas : ArrayList<Puerta> = ArrayList()
+
+        for(j in 0..matrixSala.size){
+            for(i in 0..matrixSala[j].size){
+                var objetoRecogido = matrixSala[i][j]
+                if(objetoRecogido is Puerta){
+                    puertasRecogidas.add(objetoRecogido)
+                }
+            }
+        }
+
+        //Compruevo que todas las puertas recogidas se encuentren en la lista y viceversa
+        // EN EL CASO DE QUE NO COINCIDAN NOS QUEDAMOS CON LAS QUE HAY EN LA LISTA DE PUERTAS
+        if(puertasRecogidas.size != this.puertas.size){
+            diferentes=true
+        }else{
+            // Compruevo que sean las mismas puertas
+            for (puerta:Puerta in this.puertas) {
+                if (!(puerta in puertasRecogidas)) {
+                    diferentes = true
+                }
+            }
+        }
+
+        // Si son difernetes substituimos las que hay en la lista por las de la matriz
+        if(diferentes){
+            //Substituyo las puertas que tenia por suelos
+            for(puerta in puertasRecogidas){
+                setObjetoinSala(puerta.posicion.x_sala,puerta.posicion.y_sala,Suelo(Dimension.suelo.height,Dimension.suelo.width,puerta.posicion))
+            }
+            // Pongo las que tengo en la lista a la matriz
+            for(puerta in this.puertas){
+                setObjetoinSala(puerta.posicion.x_sala,puerta.posicion.y_sala,puerta)
+            }
+        }
+
+        //Devolvemos si ha habido cambios
+        return diferentes
     }
 
     /**
      * Coloca multiples puertas
      */
     fun anadirPuertas(lista_puertas: ArrayList<Puerta>) {
+        //No solo se añade a la lista sino también a la matriz
         this.puertas.addAll(lista_puertas)
+        // Sincroniza el numero de puertas en matriz con las de la lista
+        syncPuertas()
     }
 
     //      AÑADIR ORBES
@@ -74,4 +140,63 @@ abstract class Sala(id_sala: Int, matrixSala: Array<Array<Objeto?>>) {
         this.objetos.addAll(lista_objetos)
     }
 
+    // MÉTODOS PARA ELIMINAR OBJETOS, ORBES Y PUERTAS
+
+    // ELIMINACION DE OBJETOS
+
+    /**
+     * ELimina un objeto de array que lo almazena
+     */
+    fun eliminarObjeto(objeto: Objeto){
+        this.objetos.remove(objeto)
+    }
+
+    /**
+     * Elimina múltiples objetos del array de objetos
+     */
+    fun eliminarObjetos(objetos:ArrayList<Objeto>){
+        this.objetos.removeAll(objetos)
+    }
+
+
+    // ELIMINACION DE ORBES
+    /**
+     * Elimina un orbe de la lista
+     */
+    fun eliminarOrbe(orbe: Orbe){
+        this.orbes.remove(orbe)
+    }
+
+    /**
+     * Elimina multiples orbes de la lista
+     */
+    fun eliminarOrbes(orbes:ArrayList<Orbe>){
+        this.orbes.removeAll(orbes)
+    }
+
+    // ELIMINAR PUERTAS
+    /**
+     * Elimina una puerta de la sala
+     */
+
+
+    /**
+     * Método para dibujar todos los objetos contenidos en la sala
+     */
+    fun draw(canvas: Canvas){
+        // Dibujamos suelos muros y puertas
+        for(j in 0..matrixSala.size){
+            for(i in 0..matrixSala[j].size){
+                getObjetofromSala(i,j).draw(canvas)
+            }
+        }
+        // Dibujamos objetos en la sala
+        for(objeto: Objeto in this.objetos){
+            objeto.draw(canvas)
+        }
+        // Dibujamos los orbes
+        for(orbe: Orbe in this.orbes){
+            orbe.draw(canvas)
+        }
+    }
 }
