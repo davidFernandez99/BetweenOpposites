4784b18810fc88c108b6754f5ac28c6974ce8f03
albertPerezub <albertperezcosta99@gmail.com>
2019-06-02 18:33:46 +0200

    #4 Opcions definitiu

--- app/src/main/java/grup05/pis2018/ub/edu/betweenopposites/View/Opcions.kt
+++ app/src/main/java/grup05/pis2018/ub/edu/betweenopposites/View/Opcions.kt
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
-import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.common.SignInButton
 import grup05.pis2018.ub.edu.betweenopposites.Presenter.Presenter
 import kotlinx.android.synthetic.main.activity_opcions.*
@@ -18,15 +17,12 @@ import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.api.GoogleApiClient
 
 import android.os.Vibrator
-import android.util.Log
 import com.google.android.gms.auth.api.signin.*
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
-import grup05.pis2018.ub.edu.betweenopposites.Model.UserData
 import grup05.pis2018.ub.edu.betweenopposites.R
 
 
@@ -64,6 +60,7 @@ class Opcions : AppCompatActivity(), grup05.pis2018.ub.edu.betweenopposites.View
     companion object {
         var efectos= true
         var vibracion = true
+        var musica = true
         lateinit var userId : String
 
     }
@@ -74,16 +71,34 @@ class Opcions : AppCompatActivity(), grup05.pis2018.ub.edu.betweenopposites.View
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_opcions)
 
-
-        //Mantenim el valor dels switches tot i cambiar de activities
-        val sharedPrefs = getSharedPreferences("opcions", Context.MODE_PRIVATE)
-        switch_musica.setChecked(sharedPrefs.getBoolean("swtMusica", true))
-        switch_efectos.setChecked(sharedPrefs.getBoolean("swtEfectos", true))
-        switch_vibracion.setChecked(sharedPrefs.getBoolean("swtVibracion", true))
-
         val swtMusica = findViewById<Switch>(R.id.switch_musica)
         val swtEfectos = findViewById<Switch>(R.id.switch_efectos)
         val swtVibracion = findViewById<Switch>(R.id.switch_vibracion)
+
+        txt_usuarioLetra.visibility=View.INVISIBLE
+        txt_correoLetra.visibility=View.INVISIBLE
+
+        //Controlamos estado del switch musica
+        if(musica == true){
+            swtMusica.isChecked = true
+
+        }else {
+            swtMusica.isChecked = false
+        }
+
+        //Controlamos estado del switch efectos
+        if(efectos == true){
+            swtEfectos.isChecked = true
+        }else{
+            swtEfectos.isChecked = false
+        }
+
+        //Controlamos estado del switch vibracion
+        if(vibracion == true){
+            swtVibracion.isChecked = true
+        }else{
+            swtVibracion.isChecked = false
+        }
 
 
         auth = FirebaseAuth.getInstance()
@@ -108,6 +123,8 @@ class Opcions : AppCompatActivity(), grup05.pis2018.ub.edu.betweenopposites.View
             btn_logOut.visibility=View.GONE
             txt_email.visibility=View.GONE
             txt_nomUsuari.visibility=View.GONE
+            txt_correoLetra.visibility=View.GONE
+            txt_usuarioLetra.visibility=View.GONE
         }
 
         //Switch per controlar la música
@@ -117,21 +134,16 @@ class Opcions : AppCompatActivity(), grup05.pis2018.ub.edu.betweenopposites.View
             }
             if (swtMusica.isChecked) {
                 Toast.makeText(this, "Música ON", Toast.LENGTH_SHORT).show()
-                //Agafem el valor actual del switch
-
-                val editor: SharedPreferences.Editor = getSharedPreferences("opcions", Context.MODE_PRIVATE).edit()
-                editor.putBoolean("swtMusica", true)
-                editor.commit()
+                musica = true
 
             } else {
                 Toast.makeText(this, "Música OFF", Toast.LENGTH_SHORT).show()
+                musica = false
                 //Agafem el valor actual del switch
                 if(MainActivity.player.isPlaying){
                     MainActivity.player.pause()
                 }
-                val editor = getSharedPreferences("opcions", Context.MODE_PRIVATE).edit()
-                editor.putBoolean("swtMusica", false)
-                editor.commit()
+
             }
         }
 
@@ -140,18 +152,10 @@ class Opcions : AppCompatActivity(), grup05.pis2018.ub.edu.betweenopposites.View
             if (swtEfectos.isChecked) {
                 Opcions.efectos=true
                 Toast.makeText(this, "Efectos ON", Toast.LENGTH_SHORT).show()
-                //Agafem el valor actual del switch
-                val editor: SharedPreferences.Editor = getSharedPreferences("opcions", Context.MODE_PRIVATE).edit()
-                editor.putBoolean("swtEfectos", true)
-                editor.commit()
+
             } else {
                 Opcions.efectos=false
                 Toast.makeText(this, "Efectos OFF", Toast.LENGTH_SHORT).show()
-                //Agafem el valor actual del switch
-                val editor = getSharedPreferences("opcions", Context.MODE_PRIVATE).edit()
-                editor.putBoolean("swtEfectos", false)
-                editor.commit()
-
             }
         }
 
@@ -160,20 +164,12 @@ class Opcions : AppCompatActivity(), grup05.pis2018.ub.edu.betweenopposites.View
             if (swtVibracion.isChecked) {
                 Opcions.vibracion=true
                 Toast.makeText(this, "Vibración ON", Toast.LENGTH_SHORT).show()
-                //Agafem el valor actual del switch
-                val editor: SharedPreferences.Editor = getSharedPreferences("opcions", Context.MODE_PRIVATE).edit()
-                editor.putBoolean("swtVibracion", true)
-                editor.commit()
 
                 val v: Vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator //Fem vibar el mbl al activar l'opcio
                 v.vibrate(500)
             } else {
                 Opcions.vibracion=false
                 Toast.makeText(this, "Vibración OFF", Toast.LENGTH_SHORT).show()
-                //Agafem el valor actual del switch
-                val editor = getSharedPreferences("opcions", Context.MODE_PRIVATE).edit()
-                editor.putBoolean("swtVibracion", false)
-                editor.commit()
 
             }
         }
@@ -228,6 +224,8 @@ class Opcions : AppCompatActivity(), grup05.pis2018.ub.edu.betweenopposites.View
                 Toast.makeText(this, "Sesión Iniciada", Toast.LENGTH_SHORT).show()
                 val user = auth.currentUser
                 sign_in_button.visibility=View.GONE
+                txt_usuarioLetra.visibility=View.VISIBLE
+                txt_correoLetra.visibility=View.VISIBLE
                 txt_email.text=user?.email
                 txt_nomUsuari.text=user?.displayName
                 txt_email.visibility=View.VISIBLE
@@ -259,7 +257,6 @@ class Opcions : AppCompatActivity(), grup05.pis2018.ub.edu.betweenopposites.View
 
     private fun signOut() {
         mGoogleSignInClient.signOut()
-        //FirebaseAuth.getInstance().signOut();
     }
 
 
